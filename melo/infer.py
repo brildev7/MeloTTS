40fd6a01ff744114a42528dea2917d695da9c864
brildev7 <brildev7@gmail.com>
2024-07-31 17:08:40 +0900

    Add korean model

--- melo/infer.py
+++ melo/infer.py
@@ -1,8 +1,9 @@
 import os
 import click
 from melo.api import TTS
+from datetime import datetime
 
-    
+now = str(datetime.now())
     
 @click.command()
 @click.option('--ckpt_path', '-m', type=str, default=None, help="Path to the checkpoint file")
@@ -17,7 +18,7 @@ def main(ckpt_path, text, language, output_dir):
     model = TTS(language=language, config_path=config_path, ckpt_path=ckpt_path)
     
     for spk_name, spk_id in model.hps.data.spk2id.items():
-        save_path = f'{output_dir}/{spk_name}/output.wav'
+        save_path = f'{output_dir}/{spk_name}/{now}_output.wav'
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         model.tts_to_file(text, spk_id, save_path)
 
---
+++ melo/train_commands.md
@@ -0,0 +1,7 @@
+# 데이터 전처리 실행 예
+> cd melo
+> python preprocess_text.py --metadata data/kss/metadata.list --config_path data/kss/config.json
+
+# 학습 스크립트 실행 예
+> cd melo
+> bash train_ko.sh data/kss/config.json 2

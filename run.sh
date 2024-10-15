bbd90bd178c164b15d8ab9fae0f2817eaec77bef
Seok Ju Kang <sjkang@i-screammedia.com>
2024-10-15 16:07:19 +0900

    Change run script

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+
+
+
 if [ "$1" == "start" ]; then
     echo "Starting melo tts api server.."
+
+    sleep 1    
     nohup python tts_api.py 1> /dev/null 2>&1 &
-    sleep 1
+
 elif [ "$1" == "stop" ]; then
     echo "Stopping melo tts api server.."
     sudo kill -9 `ps -ef|grep 'tts_api'|awk '{print $2}'`
-    sleep 1
+
 elif [ "$1" == "restart" ]; then
     echo "Restarting melo tts api server.."
     sudo kill -9 `ps -ef|grep 'tts_api'|awk '{print $2}'`
     sleep 3
+
+    sleep 1    
     nohup python tts_api.py 1> /dev/null 2>&1 &
 else
   echo "usage: ./start.sh start/stop/restart"
